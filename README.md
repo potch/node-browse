fb73454fb176749b41a8c54ec4a1af2a1f1b026f
Matt Claypotch <thepotch@gmail.com>
2013-11-21 17:34:11 -0800

    add files

---
+++ README.md
@@ -0,0 +1,10 @@
+# node-browse
+
+Attempts to open a URL in the default browser.
+
+## Usage
+
+```js
+var browseTo = require('node-browse').openURL;
+browseTo('http://example.com/');
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+var os = require('os');
+var spawn = require('child_process').spawn;
+var urlparse = require('url');
+
+function valid(url) {
+  var parsed = urlparse.parse(url);
+  var protocol = parsed.protocol.toLowerCase();
+  console.log(protocol);
+  if (protocol === 'http:' || protocol === 'https:') {
+    return true;
+  }
+  return false;
+}
+
+function open(url) {
+  switch(os.platform()) {
+    case 'darwin':
+      spawn('open', [url]);
+      break;
+    case 'linux':
+      spawn('xdg-open', [url]);
+      break;
+    case 'win32':
+      spawn('start', [url]);
+      break;
+  }
+}
+
+exports.openURL = function openURL(url) {
+  if (valid(url)) {
+    open(url);
+  } else {
+    console.warn('invalid protocol');
+  }
+}
